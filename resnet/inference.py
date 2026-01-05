411178c039f51e2d527d3b7bb62bf389915d8486
Leo Duan <leoduan0@icloud.com>
2026-01-05 13:45:22 -0800

    not working yet; add tqdm; optimizations

--- resnet/inference.py
+++ resnet/inference.py
@@ -20,8 +20,8 @@ from helpers import (
 ONNX_NAME = "deeplabv3_final_onnx"
 PROVIDERS = [
     # "TensorrtExecutionProvider",  # use only when on jetson
-    "NvTensorRtRtxExecutionProvider",  # use only when on desktop
     "CUDAExecutionProvider",
+    "CPUExecutionProvider",
 ]
 
 
@@ -55,7 +55,7 @@ def export_to_onnx(
     print(f"Model exported to {onnx_path}")
 
 
-def inference_real_time_test_onnx(onnx_path, providers, imgs) -> None:
+def inference_real_time_test_onnx(onnx_path, providers, imgs, device) -> None:
     """Inference using ONNX model"""
 
     print("ONNX Inference")
@@ -63,17 +63,18 @@ def inference_real_time_test_onnx(onnx_path, providers, imgs) -> None:
     # Load ONNX model
     ort_session = ort.InferenceSession(onnx_path, providers=providers)
 
-    mean = torch.tensor([0.485, 0.456, 0.406]).view(3, 1, 1)
-    std = torch.tensor([0.229, 0.224, 0.225]).view(3, 1, 1)
+    mean = torch.tensor([0.485, 0.456, 0.406], device=device).view(3, 1, 1)
+    std = torch.tensor([0.229, 0.224, 0.225], device=device).view(3, 1, 1)
 
     for img_path in imgs:
         img_raw = cv2.imread(img_path)
         if img_raw is None:
             continue
 
-        img_rgb = torch.from_numpy(img_raw[:, :, ::-1]).permute(2, 0, 1).float() / 255.0
+        img_rgb = cv2.cvtColor(img_raw, cv2.COLOR_BGR2RGB)
+        img_rgb = torch.from_numpy(img_rgb).permute(2, 0, 1).float() / 255.0
         img_tensor = ((img_rgb.to(device) - mean) / std).unsqueeze(0)
-        x = img_tensor.half().contiguous().cpu().numpy()
+        x = img_tensor.contiguous().cpu().numpy()
 
         # Run inference
         ort_inputs = {ort_session.get_inputs()[0].name: x}
@@ -117,4 +118,6 @@ if __name__ == "__main__":
         export_to_onnx(model, onnx_path=onnx_path)
 
     # Use ONNX inference
-    inference_real_time_test_onnx(onnx_path, PROVIDERS, test_imgs)
+    inference_real_time_test_onnx(
+        onnx_path, PROVIDERS, test_imgs, device=torch.device("cpu")
+    )
